a9603141f8ec424c20b89a8b0638899fef222f7a
Bsjdjxgxhdk <156095021+Bsjdjxgxhdk@users.noreply.github.com>
2024-04-16 10:12:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ api_id = 23537514
 
 api_hash = "c5ec016af7d2cb637278b715f4f5a389"
 
-bot_token = "6985503816:AAHBsK7UztuKBaOt03syoog3go9kbdHyTcc"
+bot_token = "7006565583:AAFtxxLkxes39p6D5t-coMnGrFmo8-BLRso"
 
