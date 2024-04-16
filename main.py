dd7d9834b7f2b45e8261d0c5ed1d333869d7e9d1
Bsjdjxgxhdk <156095021+Bsjdjxgxhdk@users.noreply.github.com>
2024-04-16 10:13:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ bot = Client(
     "CW",
     api_id= 23537514,
     api_hash= "c5ec016af7d2cb637278b715f4f5a389",
-    bot_token= "6985503816:AAHBsK7UztuKBaOt03syoog3go9kbdHyTcc")
+    bot_token= "7006565583:AAFtxxLkxes39p6D5t-coMnGrFmo8-BLRso")
 
 logger = logging.getLogger()
 # thumb = os.environ.get("THUMB")
